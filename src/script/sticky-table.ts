c274cf7942c504834442c031b24075d63730a5bd
levinh <lethevinh.qng@gmail.com>
2019-02-28 01:00:15 +0700

    convert to typerscript

--- src/script/sticky-table.ts
+++ src/script/sticky-table.ts
@@ -8,8 +8,6 @@ var $ = jQuery;
 
 export var CLASS = {
     "wrapper": "wrapper-sticky",
-    "wrapperLTR": "sticky-ltr-cells",
-    "wrapperRTL": "sticky-rtl-cells",
     "tableSticky": "table-sticky"
 };
 
@@ -35,16 +33,20 @@ export class StickyTable {
 
     wrapperSticky: any;
 
+    isPositionStickySupport: any;
+
     constructor(config: any) {
 
         this.config = config;
         this.tableSticky = $(config.table);
+        this.isPositionStickySupport = this.positionStickySupport();
+
         if (!this.tableSticky.length) {
             console.warn('Page is missing a sticky table region');
         }
         this.id = "stk-" + Math.random(200, 1000);
         if (!this.tableSticky.parent().hasClass(CLASS.wrapper)) {
-            this.tableSticky.wrap('<div class="' + CLASS.wrapper + ' skt-sticky-ltr-cells" id="' + this.id + '"></div>');
+            this.tableSticky.wrap('<div class="' + CLASS.wrapper + ' " id="' + this.id + '"></div>');
         }
 
         this.tableSticky.attr('data-id-stk', this.id);
@@ -72,6 +74,10 @@ export class StickyTable {
             }, 500);
         });
 
+        if (!this.isPositionStickySupport) {
+            this.handelNotPositionStickySupport();
+        }
+
         this.tableSticky.trigger(EVENTS.initSusses, { stickytable: this });
     }
 
@@ -81,6 +87,7 @@ export class StickyTable {
             var stickyTable = data.stickytable;
             stickyTable.initStickyTable();
             stickyTable.handleResponsiveSticky();
+            stickyTable.handleScrollSticky();
         });
 
         $(this.tableSticky).on(EVENTS.responsiveStart, function(event, data) {
@@ -90,7 +97,7 @@ export class StickyTable {
 
         $(this.tableSticky).on(EVENTS.scrolling, function(event, data) {
             var stickyTable = data.stickytable;
-            stickyTable.handleSupportPositionSticky();
+            stickyTable.handleScrollSticky();
         });
     };
 
@@ -100,11 +107,9 @@ export class StickyTable {
         $.each(stickies, function(position: string, stickySelector) {
             $this.tableSticky.find(stickySelector).addClass('skt-sticky-' + position);
         });
-
     };
 
     handleResponsiveSticky() {
-        console.log('response sticky');
         this.tableSticky.trigger(EVENTS.responsiveSusses, { stickytable: this });
     };
 
@@ -124,112 +129,43 @@ export class StickyTable {
 
         $(this.tableSticky).trigger(EVENTS.removeSusses, { stickytable: this });
     };
-    handleSupportPositionSticky() {
-    	
-        console.log('scrolling sticky');
-        // Multi Position Sticky
 
+    handleScrollSticky() {
 
+        // Multi Position Sticky // ???
         // Cross Browser
-
-        var positionStickySupport = this.positionStickySupport();
-
-        var scrollTypeRTL = this.scrollTypeRTL();
-
-        if (!positionStickySupport) {
-            this.handelNotPositionStickySupport();
-
-        }
-
-        if ($(CLASS.wrapperRTL).length && !(positionStickySupport && scrollTypeRTL === 'negative')) {
-
-            if (positionStickySupport) {
-                $(".st-sticky-rtl-cells table th.st-sticky-left").css('position', "relative");
-                $(".st-sticky-rtl-cells table td.st-sticky-left").css('position', "relative");
-                $(".st-sticky-rtl-cells table th.st-sticky-right").css('position', "relative");
-                $(".st-sticky-rtl-cells table td.st-sticky-right").css('position', "relative");
-
-                $(CLASS.wrapper).scroll(function() {
-                    $(this).find("table tr.st-sticky-top .st-sticky-left").css('top', $(this).scrollTop());
-                    $(this).find("table tr.st-sticky-top .st-sticky-right").css('top', $(this).scrollTop());
-                    var maxScroll = $(this).find("table").prop("clientHeight") - $(this).prop("clientHeight");
-                    $(this).find("table tr.st-sticky-bottom .st-sticky-left").css('bottom', maxScroll - $(this).scrollTop());
-                    $(this).find("table tr.st-sticky-bottom .st-sticky-right").css('bottom', maxScroll - $(this).scrollTop());
-                }).scroll();
+        var $this = this;
+        if (!this.isPositionStickySupport) {
+            if (navigator.userAgent.match(/Trident\/7\./)) {
+                $(CLASS.wrapper).on("mousewheel", function(event: any) {
+                    event.preventDefault();
+                    var wd = event.originalEvent.wheelDelta;
+                    var csp = $(this).scrollTop();
+                    $(this).scrollTop(csp - wd);
+                });
             }
-            $(CLASS.wrapperRTL).scroll(function() {
-                var maxScroll = $(this).find("table").prop("clientWidth") - $(this).prop("clientWidth");
-                switch (scrollTypeRTL) {
-                    case "default": // webKit Browsers
-                        $(this).find("table th.st-sticky-left").css('right', maxScroll - $(this).scrollLeft());
-                        $(this).find("table td.st-sticky-left").css('right', maxScroll - $(this).scrollLeft());
-                        $(this).find("table th.st-sticky-right").css('left', $(this).scrollLeft());
-                        $(this).find("table td.st-sticky-right").css('left', $(this).scrollLeft());
-                        break;
-                    case "negative": // Firefox, Opera
-                        $(this).find("table th.st-sticky-left").css('right', $(this).scrollLeft() * -1);
-                        $(this).find("table td.st-sticky-left").css('right', $(this).scrollLeft() * -1);
-                        $(this).find("table th.st-sticky-right").css('left', maxScroll + $(this).scrollLeft());
-                        $(this).find("table td.st-sticky-right").css('left', maxScroll + $(this).scrollLeft());
-                        break;
-                    case "reverse": // IE, Edge
-                        $(this).find("table th.st-sticky-left").css('right', $(this).scrollLeft());
-                        $(this).find("table td.st-sticky-left").css('right', $(this).scrollLeft());
-                        $(this).find("table th.st-sticky-right").css('left', maxScroll - $(this).scrollLeft());
-                        $(this).find("table td.st-sticky-right").css('left', maxScroll - $(this).scrollLeft());
-                }
-            }).scroll();
+
+            $(this.tableSticky).on(EVENTS.scrolling, function(event, data) {
+                var stickyTable = data.stickytable;
+                stickyTable.handelNotPositionStickySupport();
+            });
         }
-    };
-
-    handleSupportScrollTypeRTL() {
-
     };
 
     handelNotPositionStickySupport() {
-        if (navigator.userAgent.match(/Trident\/7\./)) {
-            $(CLASS.wrapper).on("mousewheel", function(event: any) {
-                event.preventDefault();
-                var wd = event.originalEvent.wheelDelta;
-                var csp = $(this).scrollTop();
-                $(this).scrollTop(csp - wd);
-            });
-        }
+        this.wrapperSticky.find("table tr.skt-sticky-top th").css('top', this.wrapperSticky.scrollTop());
+        this.wrapperSticky.find("table tr.skt-sticky-top td").css('top', this.wrapperSticky.scrollTop());
+        var maxScroll = this.wrapperSticky.find("table").prop("clientHeight") - this.wrapperSticky.prop("clientHeight");
+        this.wrapperSticky.find("table tr.skt-sticky-bottom th").css('bottom', maxScroll - this.wrapperSticky.scrollTop());
+        this.wrapperSticky.find("table tr.skt-sticky-bottom td").css('bottom', maxScroll - this.wrapperSticky.scrollTop());
 
-        $(CLASS.wrapper).scroll(function() {
-            $(this).find("table tr.st-sticky-top th").css('top', $(this).scrollTop());
-            $(this).find("table tr.st-sticky-top td").css('top', $(this).scrollTop());
-            var maxScroll = $(this).find("table").prop("clientHeight") - $(this).prop("clientHeight");
-            $(this).find("table tr.st-sticky-bottom th").css('bottom', maxScroll - $(this).scrollTop());
-            $(this).find("table tr.st-sticky-bottom td").css('bottom', maxScroll - $(this).scrollTop());
-        }).scroll();
-
-        $(CLASS.wrapperLTR).scroll(function() {
-            $(this).find("table th.st-sticky-left").css('left', $(this).scrollLeft());
-            $(this).find("table td.st-sticky-left").css('left', $(this).scrollLeft());
-            var maxScroll = $(this).find("table").prop("clientWidth") - $(this).prop("clientWidth");
-            $(this).find("table th.st-sticky-right").css('right', maxScroll - $(this).scrollLeft());
-            $(this).find("table td.st-sticky-right").css('right', maxScroll - $(this).scrollLeft());
-        }).scroll();
+        this.wrapperSticky.find("table th.skt-sticky-left").css('left', this.wrapperSticky.scrollLeft());
+        this.wrapperSticky.find("table td.skt-sticky-left").css('left', this.wrapperSticky.scrollLeft());
+        var maxScroll = this.wrapperSticky.find("table").prop("clientWidth") - this.wrapperSticky.prop("clientWidth");
+        this.wrapperSticky.find("table th.skt-sticky-right").css('right', maxScroll - this.wrapperSticky.scrollLeft());
+        this.wrapperSticky.find("table td.skt-sticky-right").css('right', maxScroll - this.wrapperSticky.scrollLeft());
 
     };
-
-    scrollTypeRTL() {
-        var definer = $('<div dir="rtl" style="font-size: 14px; width: 4px; height: 1px; position: absolute; top: -1000px; overflow: scroll">ABCD</div>').appendTo('body')[0],
-            scrollTypeRTL = 'reverse';
-
-        if (definer.scrollLeft > 0) {
-            scrollTypeRTL = 'default';
-        } else {
-            definer.scrollLeft = 1;
-            if (definer.scrollLeft === 0) {
-                scrollTypeRTL = 'negative';
-            }
-        }
-        $(definer).remove();
-        return scrollTypeRTL;
-    };
-
 
     positionStickySupport() {
         var el = document.createElement('a'),
@@ -237,5 +173,4 @@ export class StickyTable {
         mStyle.cssText = "position:sticky;position:-webkit-sticky;position:-ms-sticky;";
         return mStyle.position.indexOf('sticky') !== -1;
     };
-
 }
